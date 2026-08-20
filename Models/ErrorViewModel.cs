887850de206217346ef2b368a8a75f5644fa7ccc
EITAN ARIEL GLAZ <49876257@ort>
2026-08-20 08:41:39 -0300

    subida inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Sala_de_escape.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
